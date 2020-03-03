30af798476402cffcbd071acddf44a9d773c6b43
SpiralP <spiral.palmer@gmail.com>
2020-03-02 22:52:41 -0800

    cleanup test

--- tests/test.rs
+++ tests/test.rs
@@ -2,28 +2,16 @@ use win_toast::*;
 
 #[test]
 fn it_works() {
-  ag();
-}
-
-fn ag() {
-  // std::thread::spawn(move || {
-  //   std::thread::sleep(std::time::Duration::from_secs(30));
-  //   let _ = WAIT.0.send(());
-  // });
-
   extern "C" fn activated(action_index: i32) {
     println!("activated {:?}", action_index);
-    // let _ = WAIT.0.send(());
   }
 
   extern "C" fn dismissed(state: IWinToastHandler_WinToastDismissalReason) {
     println!("dismissed {:?}", state);
-    // let _ = WAIT.0.send(());
   }
 
   extern "C" fn failed() {
     println!("failed");
-    // let _ = WAIT.0.send(());
   }
 
   let mut win_toast = WinToast::initialize("Test App", "Company", "App Name").unwrap();
@@ -37,6 +25,4 @@ fn ag() {
   let handler = WinToastHandler::new(activated, dismissed, failed);
 
   win_toast.show_toast(&template, &handler).unwrap();
-
-  println!("end");
 }
